984be0f437ee4bdb539807754baa6efb1c9c3774
omarraam <omarkeyow318@gmail.com>
2025-04-07 14:34:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Omarraam',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Isra Bot",
+  botOwner: 'Omarraam Robot', // Your name
+  ownerNumber: '+254794458188', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
